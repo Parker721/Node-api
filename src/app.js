b6c5f172d6da86da52f478001527750b30a32894
Daniel Guerrero <porta11271127@gmail.com>
2022-06-06 19:02:16 -0600

    adding cors

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ import userRouter from './routes/routes.user.js'
 import loginRouter from './routes/routes.login.js'
 import handleError  from './middleware/handleError.js'
 import notFound from './middleware/notFound.js'
+import cors from 'cors'
 
-
-const app = express();
-
+const app= express()
+app.use(express.json())
+app.use(cors())
 app.use(loginRouter);
 app.use(notesRoute)
 app.use(userRouter)
 app.use(handleError)
 app.use(notFound)
-
 
 export default app
